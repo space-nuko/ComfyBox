7cbb5b0c2ea228ea39d25762111fbc3c7697c7c9
space-nuko <24979496+space-nuko@users.noreply.github.com>
2023-05-21 18:50:27 -0500

    Converted seed widget test

--- src/tests/convertVanillaWorkflowTests.ts
+++ src/tests/convertVanillaWorkflowTests.ts
@@ -11,7 +11,8 @@ import type { ComfyNodeDef } from '$lib/ComfyNodeDef';
 
 const objectInfo: Record<string, ComfyNodeDef> = await import("./data/objectInfo.json")
 const json1: ComfyVanillaWorkflow = await import("./data/convertedWidget.json")
-const json2: ComfyVanillaWorkflow = await import("./data/convertedWidgetAndPrimitiveNode.json")
+const json2: ComfyVanillaWorkflow = await import("./data/convertedSeedWidget.json")
+const json3: ComfyVanillaWorkflow = await import("./data/convertedWidgetAndPrimitiveNode.json")
 
 export default class convertVanillaWorkflowTests extends UnitTest {
     test__convertsWidget() {
@@ -53,9 +54,56 @@ export default class convertVanillaWorkflowTests extends UnitTest {
         expect(convWorkflow.graph.getNodeById(widget.node.id)).toEqual(widget.node)
     }
 
+    test__convertsSeedWidget() {
+        const workflow = LiteGraph.cloneObject(json2)
+        const attrs: WorkflowAttributes = { ...defaultWorkflowAttributes }
+
+        ComfyApp.knownBackendNodes["KSampler"] = {
+            nodeDef: objectInfo["KSampler"]
+        }
+
+        const converted = convertVanillaWorkflow(workflow, attrs)
+
+        expect(converted).toBeInstanceOf(Array)
+
+        const [convWorkflow, convLayout] = converted;
+
+        const layout = get(convLayout)
+
+        expect(Object.keys(layout.allItems)).toHaveLength(10)
+
+        const widgets = Object.values(layout.allItems).filter(di => di.dragItem.type === "widget").map(di => di.dragItem) as WidgetLayout[];
+        expect(widgets).toHaveLength(6);
+
+        const widgetsValues = widgets.map(w => { return [w.node.type, w.node.getValue(), w.attrs.title] })
+        expect(widgetsValues).toEqual([
+            ["ui/number", 20, 'steps'],
+            ["ui/number", 8, 'cfg'],
+            ["ui/combo", 'euler', 'sampler_name'],
+            ["ui/combo", 'normal', 'scheduler'],
+            ["ui/number", 1, 'denoise'],
+            ["ui/number", 1461, 'seed']
+        ]);
+
+        const widget = widgets.find(w => w.attrs.title === "seed") as WidgetLayout | null;
+        expect(widget).toBeDefined();
+        expect(widget.node).toBeDefined();
+        expect(widget.node.type).toEqual("ui/number")
+        expect(widget.node.getValue()).toEqual(1461)
+        expect(convWorkflow.graph.getNodeById(widget.node.id)).toEqual(widget.node)
+
+        const links = widget.node.getOutputLinks(0)
+        expect(links).toHaveLength(1);
+
+        const kSampler = convWorkflow.graph.findNodesByType("KSampler")[0];
+        expect(links[0].origin_id).toEqual(widget.node.id);
+        expect(links[0].target_id).toEqual(kSampler.id);
+        expect(widget.node.outputs[0].type).toEqual("number");
+        expect(links[0].type).toEqual("number");
+    }
 
     test__convertsPrimitiveNodeAndConvertedInput() {
-        const workflow = LiteGraph.cloneObject(json2)
+        const workflow = LiteGraph.cloneObject(json3)
         const attrs: WorkflowAttributes = { ...defaultWorkflowAttributes }
 
         ComfyApp.knownBackendNodes["KSampler"] = {
