9944ed869ca4bd4efaf453fa7f1e68a4873b293d
ChessScholar <65353254+ChessScholar@users.noreply.github.com>
2024-01-09 19:54:10 -0500

    Updated .bat
    This has some extra information for the end-user to ensure they have set the other file "run_nvidia_gpu.bat" with the argument "--enable-cors-header". Without this, then Comfybox will not be able to see the ComfyUI server.

--- start_servers.bat
+++ start_servers.bat
@@ -1,27 +1,32 @@
 @echo off
 setlocal enabledelayedexpansion
 
+echo Starting run_nvidia_gpu.bat (please make sure it has the argument --enable-cors-header). 
+
 REM Change to the backend directory
-cd "..\ComfyUI"
+cd ".."
 
 REM Start the backend server
-start /b cmd /c python main.py --enable-cors-header > ..\backend_log.txt
+start "" "run_nvidia_gpu.bat"
 
-REM Wait for the backend server to be ready
-:waitForBackend
+echo Waiting for the server to start...
+echo Note: You may increase or decrease the timeout time by changing its value (Default: 10 seconds).
 timeout /t 1 > NUL
-findstr /m "Starting server" "..\backend_log.txt"
-if errorlevel 1 goto waitForBackend
+
+timeout /t 10
+
 
 REM Start the frontend server
-cd "..\ComfyBox-ReOpened-Forked-"
+cd "ComfyBox-ReOpened-Forked-"
 start run.bat
 
 REM Optional: Insert a delay here to allow some time for the frontend to start
-REM timeout /t 10
+REM timeout /t 5
 
-REM Open the frontend in the default web browser
-start "" "http://127.0.0.1:8000/"
+start "" "http://localhost:8000/"
 
 echo Both servers are running.
+echo You may now close this window.
+
 pause
+
